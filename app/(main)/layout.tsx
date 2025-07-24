301b0dfc5e6bff514bc20e2836c308917d8dff7f
Ibrahim Raimi <raimiibrahim44@gmail.com>
2025-07-24 18:29:18 +0100

    refactor: Remove unused layout and play page components to improve code maintainability

--- app/(main)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import MainLayout from "@/components/layout/main-layout";
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <MainLayout>{children}</MainLayout>;
-}
